726f3c28585fc8f1b8e8e1424fac89f5aaa2d0d8
ryant26 <ryant26@gmail.com>
2014-11-26 13:36:42 -0700

    first

---
+++ src/Client/main.java
@@ -0,0 +1,17 @@
+package Client;
+
+
+public class main
+{
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args)
+	{
+
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/Server/main.java
@@ -0,0 +1,17 @@
+package Server;
+
+
+public class main
+{
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args)
+	{
+
+		// TODO Auto-generated method stub
+
+	}
+
+}
